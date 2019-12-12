aad74fa2084812c0cde0658ca6c3f46f611a5125
Bryce-7034 <brkelly6585@gmail.com>
2019-12-12 09:53:30 -0800

    Zombie dice

--- ZombieDiePane.java
+++ ZombieDiePane.java
@@ -1,6 +1,36 @@
-public class ZombieDiePane {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
+import javafx.scene.text.Font;
+
+import java.awt.*;
+import java.net.URL;
+
+public class ZombieDiePane extends GridPane{
 
     public ZombieDiePane(){
+        setAlignment(Pos.CENTER);
+        setHgap(20);
+        setVgap(10);
+        setStyle("-fx-background-color: blue");
 
+
+        /*
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("/mainScene.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+        */
     }
 }
