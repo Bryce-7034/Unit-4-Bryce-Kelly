cfea9d8c22cd85438bc8693944b66b1ae657803f
Bryce-7034 <brkelly6585@gmail.com>
2019-12-12 09:54:12 -0800

    Zombie dice

---
+++ NumberOfPlayers.java
@@ -0,0 +1,9 @@
+public class NumberOfPlayers {
+    private int playercount;
+    public NumberOfPlayers(int numPlayers){
+        playercount = numPlayers;
+    }
+    public int players(){
+        return playercount;
+    }
+}
